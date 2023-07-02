6276ce7f5db7a3e46f7c6b57b8c40af723c47499
mafu <mafusukee@gmail.com>
2023-07-03 02:22:57 +0900

    fix not working on colab

--- googlecolab.py
+++ googlecolab.py
@@ -2,8 +2,5 @@ from webdler.webdler import download_page
 from IPython.core.display import HTML
 
 url = ""  # @param {type:"string"}
-if not url:
-    print("Please enter a valid url!!")
-else:
-    html_path = download_page(url)
-    HTML(filename=html_path)
+html_path = download_page(url)
+HTML(filename=html_path)
