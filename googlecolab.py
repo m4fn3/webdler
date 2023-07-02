bbb50abeab01802b3397775b47ea6f46d25c9b07
mafu <mafusukee@gmail.com>
2023-07-03 02:16:28 +0900

    added google colab specific UI

--- googlecolab.py
+++ googlecolab.py
@@ -1,6 +1,9 @@
-from webdler import download_page
+from webdler.webdler import download_page
 from IPython.core.display import HTML
 
 url = ""  # @param {type:"string"}
-html_path = download_page(url)
-HTML(filename=html_path)
+if not url:
+    print("Please enter a valid url!!")
+else:
+    html_path = download_page(url)
+    HTML(filename=html_path)
